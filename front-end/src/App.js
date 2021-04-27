502685da2fd741bf9d3e04fce0c082254a9af0a1
alissonjosef <alissonjosef@gmail.com>
2021-04-27 16:27:33 -0300

    api

---
+++ front-end/src/App.js
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import api from './services/api';
+import './App.css';
+import logo from './assets/Logo.svg'
+
+function App() {
+  const [email, setEmail] = useState('');
+
+  function handleSubmit(event){
+    event.preventDefault();
+    console.log(email);
+    
+  }
+
+  function handleEmailChange(event){
+    setEmail(event.target.value)
+  }
+  return (
+    <div className="container">
+      <img src={logo} alt="Correitaoutlet"/>
+
+      <div className="content">
+        <p>
+          Encontre a sua <strong>melhor</strong> forma de <strong>vestir</strong> com a melhor que ta tendo.
+        </p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-mail</label>
+          <input type="email"
+           id="email" 
+           placeholder="Seu melhor email"
+           value={email}
+           onChange={handleEmailChange}
+           />
+
+           <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default App;
