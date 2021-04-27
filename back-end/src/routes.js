f53cb026de70a9cb2f02a6fee31ce1fc8bfda110
alissonjosef <alissonjosef@gmail.com>
2021-04-27 11:09:50 -0300

    booking_Final

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -3,14 +3,20 @@ const multer = require('multer');
 const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
-const SpotControllers = require('./controllers/SpotController');
+const SpotController = require('./controllers/SpotController');
+const DashboardController = require('./controllers/DashboardController');
+const BookingController = require('./controllers/BookingController');
 
 const routes = express.Router();
 const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 
-routes.get('/spot',SpotControllers.index);
-routes.post('/spot', upload.single('thumbnail'),SpotControllers.store);
+routes.get('/spot',SpotController.index);
+routes.post('/spot', upload.single('thumbnail'),SpotController.store);
+
+routes.get('/dashboard',DashboardController.show);
+
+routes.post('/spot/:spot_id/bookings',BookingController.store);
 
 module.exports = routes;
