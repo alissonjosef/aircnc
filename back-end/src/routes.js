8e219a35db4f0f2b64a9ac8166941abe22dfcf68
alissonjosef <alissonjosef@gmail.com>
2021-04-27 09:08:38 -0300

    Spot_index

--- back-end/src/routes.js
+++ back-end/src/routes.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const multer = require('multer');
-const uploadConfig = require('./config/uplouad');
+const uploadConfig = require('./config/upload');
 
 const SessionController = require('./controllers/SessionController');
-const SporControllers = require('./controllers/SpotController');
+const SpotControllers = require('./controllers/SpotController');
 
 const routes = express.Router();
-const uplouad = multer(uploadConfig);
+const upload = multer(uploadConfig);
 
 routes.post('/sessions', SessionController.store);
 
-routes.get('/spot',SporControllers.index);
-routes.post('/spot', uplouad.single('thumbnail'),SporControllers.store);
+routes.get('/spot',SpotControllers.index);
+routes.post('/spot', upload.single('thumbnail'),SpotControllers.store);
 
 module.exports = routes;
